d818c6d50ae02dcbcaca4e429c9fc5f579860283
Mark Cruz <markchriscruz@yahoo.com>
2018-03-09 00:01:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing\b/)
+  return text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
