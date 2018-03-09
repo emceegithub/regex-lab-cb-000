030747544abbd4cba38b54dbd1cd6a6404e4ec13
Mark Cruz <markchriscruz@yahoo.com>
2018-03-09 00:10:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,11 +13,10 @@ end
 
 def words_five_letters_long(text)
   return text.scan(/\b[a-z]{5}\b/)
-  
 end
   
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
